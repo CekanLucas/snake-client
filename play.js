2d1522c2f85723d30bfb533644f2a0f73a6a1bb1
Lucas <cekanlucas@gmail.com>
2020-02-12 14:01:19 -0500

    refactored connect function to client.js

--- play.js
+++ play.js
@@ -1,24 +1,6 @@
 const net = require('net');
 const chalk = require('chalk');
+const {connect}= require('./client');
 
-/**
- * Establishes connection with the game server
- */
-const connect = function() {
-  const conn = net.createConnection({ 
-    host: '172.46.0.236',
-    port: 50541
-  });
-  // interpret incoming data as text
-  conn.setEncoding('utf8'); 
-  conn.on('connect',
-    data => {
-      const msgData = chalk.yellow('Data received: ');
-      console.log(msgData,chalk.red(data));
-    }
-  )
-  return conn;
-}
-
-console.log('Connecting ...');
+console.log(chalk.cyan('Connecting...'));
 connect();
