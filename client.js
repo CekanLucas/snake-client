c26c795e7e8ab6e12b8682cb91015ca0ac387587
Lucas Cekan <cekanlucas@gmail.com>
2025-07-13 03:30:35 +0000

    moved connect function from play to client .js

--- client.js
+++ client.js
@@ -1,43 +1,59 @@
 const net = require('net');
 const chalk = require('chalk');
-const {IP, PORT} = require('./constants');
+const { IP, PORT } = require('./constants');
 
 /**
  * Establishes connection with the game server
  */
+// const connect = function () {
+//   const conn = net.createConnection({
+//     host: IP,
+//     port: PORT
+//   });
+
+//   conn.setEncoding('utf8'); // interpret incoming data as text
+
+//   conn.on('connect', //event triggers when connection first established
+//     () => {
+//       const msg = chalk.blackBright('Connection has been established');
+//       console.log(msg);
+//       conn.write("Name: LIC");
+
+//       // send data on connection
+//       conn.on('connection', //huh?
+//         data => {
+//           console.log('connection')
+//           const msgData = chalk.yellow('Data received: ');
+//           console.log(msgData, chalk.red(data));
+//         }
+//       )
+//     }
+//   )
+
+//   return conn;
+// }
+
 const connect = function() {
-  const conn = net.createConnection({ 
-    host: IP,
-    port: PORT
+  const conn = net.createConnection({
+    host: 'localhost',
+    port: 50541
   });
-  // interpret incoming data as text
-  conn.setEncoding('utf8'); 
+  
+  conn.setEncoding("utf-8");
 
-  conn.on('connect', //event triggers when connection first established
-    () => {
-      const msg = chalk.blackBright('Connection has been established');
-      console.log(msg);
-      conn.write("Name: LIC");
-      
-      // send data on connection
-      conn.on('connection', //huh?
-        data => {
-          console.log('connection')
-          const msgData = chalk.yellow('Data received: ');
-          console.log(msgData,chalk.red(data));
-        }
-      )
-    }
-  )
-  return conn;
+  conn.on('data', data => {
+    console.log('Incoming data: ', data);
+  })
+  
+  return conn
 }
 
-let i = 0; 
-const numConn = 2
-while(i < numConn){
-  connect();
-  i++;
-}
+// let i = 0;
+// const numConn = 2
+// while (i < numConn) {
+//   connect();
+//   i++;
+// }
 
 module.exports = {
   connect
--- play.js
+++ play.js
@@ -1,25 +1,9 @@
 const net = require('net');
-// const chalk = require('chalk');
-// const {connect}= require('./client');
+const chalk = require('chalk');
+const {connect}= require('./client');
 // const { setupInput } = require('./input');
 
 // setupInput(connect());
 
-const connect = function() {
-  const conn = net.createConnection({
-    host: 'localhost',
-    port: 50541
-  });
-  
-  conn.setEncoding("utf-8");
-
-  conn.on('data', data => {
-    console.log('Incoming data: ', data);
-  })
-  
-  return conn
-}
-
-// console.log(chalk.cyan('Connecting...'));
-console.log('Connecting...');
+console.log(chalk.cyan('Connecting...'));
 connect();
